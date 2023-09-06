ff52d1ecd1e777f4ae6891cd4481520664fda1fa
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-06 15:56:59 +0300

    commit

---
+++ src/app/auth/auth.guards.ts
@@ -0,0 +1,22 @@
+// src/app/auth/auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Token exists, user is authenticated
+      return true;
+    } else {
+      // Redirect to login page
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
