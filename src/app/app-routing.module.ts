6210b1b06fe73cab7c66cbcde136f15a990b763f
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-07 11:41:03 +0300

    starting over

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'videos', component: VideoListComponent },
-  { path: 'video/:id', component: VideoPlayerComponent },
-  { path: 'video/:id/pre-test', component: VideoTestComponent },
-  { path: 'video/:id/post-test', component: VideoTestComponent },
-  { path: 'unit-test/:id', component: UnitTestComponent },
-  // Add more routes as needed
-];
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
