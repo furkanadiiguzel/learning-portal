74bf83ede04cce13dfe313a2f86e2745695ee787
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-07 11:00:12 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'login', component: LoginComponent },
+  { path: 'videos', component: VideoListComponent },
+  { path: 'video/:id', component: VideoPlayerComponent },
+  { path: 'video/:id/pre-test', component: VideoTestComponent },
+  { path: 'video/:id/post-test', component: VideoTestComponent },
+  { path: 'unit-test/:id', component: UnitTestComponent },
+  // Add more routes as needed
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
