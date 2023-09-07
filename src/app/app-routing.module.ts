618719ed5b80aeed434daf32ea27b0f60ac75fcd
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-07 15:56:56 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), NzDrawerModule], // Import NzDrawerModule
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+
