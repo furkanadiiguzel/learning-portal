00f8704fc28b796620017eca479779e6efbc3245
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-07 16:11:34 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { AppComponent } from './app.component';
+import { NzDemoDrawerPlacementComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to the home route
+  { path: 'home', component: AppComponent }, // Route to the AppComponent
+  { path: 'drawer', component: NzDemoDrawerPlacementComponent }, // Route to the NzDemoDrawerPlacementComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), NzDrawerModule], // Import NzDrawerModule
-  exports: [RouterModule],
-})
+  exports: [RouterModule],})
 export class AppRoutingModule { }
 
