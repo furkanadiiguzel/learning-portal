f8bb6eae2ed1640bcb58e2f006bebc6b494b1c92
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-09 16:10:13 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
@@ -24,7 +22,7 @@ registerLocaleData(en);
     BrowserAnimationsModule
   ],
   providers: [
-    { provide: NZ_I18N, useValue: en_US }
+   
   ],
   bootstrap: [AppComponent]
 })
