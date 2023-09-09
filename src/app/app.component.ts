b243c2e61ca8393479183431cf4cbb0bfb5b0659
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-09 16:08:51 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
 
 
 @Component({
@@ -8,43 +7,8 @@ import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
   styleUrls: ['./app.component.css']
 })
 
-
-@Component({
-  selector: 'nz-demo-drawer-placement',
-  template: `
-    <nz-radio-group [(ngModel)]="placement">
-      <label nz-radio nzValue="top">top</label>
-      <label nz-radio nzValue="right">right</label>
-      <label nz-radio nzValue="bottom">bottom</label>
-      <label nz-radio nzValue="left">left</label>
-    </nz-radio-group>
-    <button nz-button nzType="primary" (click)="open()">Open</button>
-    <nz-drawer
-      [nzClosable]="false"
-      [nzVisible]="visible"
-      [nzPlacement]="placement"
-      nzTitle="Basic Drawer"
-      (nzOnClose)="close()"
-    >
-      <ng-container *nzDrawerContent>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </ng-container>
-    </nz-drawer>
-  `
-})
 export class AppComponent {
   title = 'learning-portal';
 }
-export class NzDemoDrawerPlacementComponent {
-  visible = false;
-  placement: NzDrawerPlacement = 'left';
-  open(): void {
-    this.visible = true;
-  }
 
-  close(): void {
-    this.visible = false;
-  }
-}
+
