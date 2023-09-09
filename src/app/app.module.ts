ac428efa5fd757a80b2f87750394ac72505ff85f
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-09 17:39:26 +0300

    all files updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+
 import { AppRoutingModule } from './app-routing.module';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-
-registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent
-
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-
-
+    AppRoutingModule
   ],
-  providers: [
-   
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
