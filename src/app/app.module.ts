40a90cbf3852b5da0b21187cc2c4f229c204216e
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-29 00:50:19 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { SignupComponent } from './components/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
