6115ff6e2667679baf2c631596eedd25e280d908
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-28 17:35:01 +0300

    commit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
+import { UserStoreService } from 'src/app/services/user-store.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-  constructor(private auth: AuthService){}
+export class DashboardComponent implements OnInit{
+  public fullname : string = "";
+  constructor(private auth: AuthService, private userStore: UserStoreService){}
+  ngOnInit(){
+    this.userStore.getFullNameFromStore()
+    .subscribe(val=>{
+      let FullNameFromToken = this.auth.getFullNameFromToken();
+      this.fullname = val || FullNameFromToken;
+  });
+
+  }
+
 
   logout(){
     this.auth.signOut();
-
   }
 
 
